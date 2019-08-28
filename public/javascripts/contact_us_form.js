53ef2c1bda962a526fa86e152c1ffe9d7b07f18b
sktocha <antonzlatogor@gmail.com>
2019-08-28 14:02:07 +0300

    clean from unnecessary returns in contact_us_form + put and comment original coffeescript code

--- public/javascripts/contact_us_form.js
+++ public/javascripts/contact_us_form.js
@@ -16,30 +16,30 @@ ContactUsForm = (function() {
         data = new FormData(_this.form.get(0));
         _this.disableForm();
         if (window.environment === 'test') {
-          return _this.submitForm(data);
+          _this.submitForm(data);
         } else {
           return grecaptcha.ready(function() {
             return grecaptcha.execute(window.captcha_site_key, {
               action: 'create_comment'
             }).then(function(token) {
               data.set('g-recaptcha-response', token);
-              return _this.submitForm(data);
+              _this.submitForm(data);
             });
           });
         }
       };
     })(this));
-    return this.form.find('#send_more').bind('click', (function(_this) {
+    this.form.find('#send_more').bind('click', (function(_this) {
       return function(e) {
         e.preventDefault();
         _this.form.find('#success').hide();
-        return _this.form.find('fieldset').show();
+        _this.form.find('fieldset').show();
       };
     })(this));
   };
 
   ContactUsForm.prototype.submitForm = function(data) {
-    return $.post({
+    $.post({
       url: this.form.attr('action'),
       data: data,
       processData: false,
@@ -48,18 +48,18 @@ ContactUsForm = (function() {
       return function(xhr, data) {
         _this.form.find('#success').show();
         _this.form.find('fieldset').hide();
-        return _this.resetForm();
+        _this.resetForm();
       };
     })(this)).fail((function(_this) {
       return function(xhr) {
         _this.markAllFieldsAsSuccess();
-        return $.each(xhr.responseJSON, function(k, v) {
-          return _this.markFieldAsInvalid(k, v);
+        $.each(xhr.responseJSON, function(k, v) {
+          _this.markFieldAsInvalid(k, v);
         });
       };
     })(this)).always((function(_this) {
       return function(xhr) {
-        return _this.enableForm();
+        _this.enableForm();
       };
     })(this));
   };
@@ -67,27 +67,27 @@ ContactUsForm = (function() {
   ContactUsForm.prototype.resetForm = function() {
     this.form.trigger('reset');
     this.markAllFieldsAsSuccess();
-    return this.form.find('input, label, textarea').removeClass('is-valid text-success');
+    this.form.find('input, label, textarea').removeClass('is-valid text-success');
   };
 
   ContactUsForm.prototype.markAllFieldsAsSuccess = function() {
     this.form.find('input,textarea').removeClass('is-invalid').addClass('is-valid');
     this.form.find('label').removeClass('text-danger').addClass('text-success');
-    return this.form.find('small[id$="_error"]').text('');
+    this.form.find('small[id$="_error"]').text('');
   };
 
   ContactUsForm.prototype.markFieldAsInvalid = function(field_id, error) {
     this.form.find("#email_message_" + field_id).removeClass('is-valid').addClass('is-invalid');
     this.form.find("label[for=email_message_" + field_id + "]").removeClass('text-success').addClass('text-danger');
-    return this.form.find("#email_message_" + field_id + "_error").text(error);
+    this.form.find("#email_message_" + field_id + "_error").text(error);
   };
 
   ContactUsForm.prototype.enableForm = function() {
-    return this.form.find('fieldset').removeAttr('disabled');
+    this.form.find('fieldset').removeAttr('disabled');
   };
 
   ContactUsForm.prototype.disableForm = function() {
-    return this.form.find('fieldset').attr('disabled', '1');
+    this.form.find('fieldset').attr('disabled', '1');
   };
 
   return ContactUsForm;
@@ -95,5 +95,61 @@ ContactUsForm = (function() {
 })();
 
 $(function() {
-  return window.main_form = new ContactUsForm();
+  window.main_form = new ContactUsForm();
 });
+
+
+// original(coffeescript)
+
+// class ContactUsForm
+//   constructor: ->
+//     @form = $('#main_form')
+//     @initHandlers()
+//   initHandlers: ->
+
+//     @form.bind 'submit', (e) =>
+//       e.preventDefault()
+//       data = new FormData(@form.get(0))
+//       @disableForm()
+//       if window.environment == 'test'
+//         @submitForm(data)
+//       else
+//         grecaptcha.ready =>
+//           grecaptcha.execute(window.captcha_site_key, {action: 'create_comment'}).then (token) =>
+//             data.set('g-recaptcha-response', token)
+//             @submitForm(data)
+
+//     @form.find('#send_more').bind 'click', (e) =>
+//       e.preventDefault()
+//       @form.find('#success').hide()
+//       @form.find('fieldset').show()
+
+//   submitForm: (data) ->
+//     $.post(url: @form.attr('action'), data: data, processData: false, contentType: false)
+//       .done (xhr, data) =>
+//         @form.find('#success').show()
+//         @form.find('fieldset').hide()
+//         @resetForm()
+//     .fail (xhr) =>
+//       @markAllFieldsAsSuccess()
+//       $.each xhr.responseJSON, (k, v) =>
+//         @markFieldAsInvalid(k, v)
+//     .always (xhr) =>
+//       @enableForm()
+//   resetForm: ->
+//     @form.trigger('reset')
+//     @markAllFieldsAsSuccess()
+//     @form.find('input, label, textarea').removeClass('is-valid text-success')
+//   markAllFieldsAsSuccess: ->
+//     @form.find('input,textarea').removeClass('is-invalid').addClass('is-valid')
+//     @form.find('label').removeClass('text-danger').addClass('text-success')
+//     @form.find('small[id$="_error"]').text('')
+//   markFieldAsInvalid: (field_id, error) =>
+//      @form.find("#email_message_#{field_id}").removeClass('is-valid').addClass('is-invalid')
+//      @form.find("label[for=email_message_#{field_id}]").removeClass('text-success').addClass('text-danger')
+//      @form.find("#email_message_#{field_id}_error").text(error)
+//   enableForm: ->
+//     @form.find('fieldset').removeAttr('disabled')
+//   disableForm: ->
+//     @form.find('fieldset').attr('disabled', '1')
+// $ -> window.main_form = new ContactUsForm()
